48fae1e6c01b106a8c888cfecf6610cdcda730fe
Alkalouti <a.alkalouti@gmail.com>
2015-08-21 14:56:38 +0300

    Initial Commit

---
+++ Marked/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Marked
+//
+//  Created by AlKalouti on 8/21/15.
+//  Copyright (c) 2015 company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Marked/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Marked
+//
+//  Created by AlKalouti on 8/21/15.
+//  Copyright (c) 2015 company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
